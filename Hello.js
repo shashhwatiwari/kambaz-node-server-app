de11d4bba51771245d09adb405d6236b568a286b
Shashwat Tiwari <106163122+shashhwatiwari@users.noreply.github.com>
2025-08-04 03:02:53 +0530

    Lab 5 work - node server end

--- Hello.js
+++ Hello.js
@@ -1,2 +1,10 @@
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => { res.send('Welcome to Full Stack Development!')})
+export default function Hello(app) {
+  const sayHello = (req, res) => {
+    res.send("Life is good!");
+  };
+  const sayWelcome = (req, res) => {
+    res.send("Welcome to Full Stack Development!");
+  };
+  app.get("/hello", sayHello);
+  app.get("/", sayWelcome);
+}

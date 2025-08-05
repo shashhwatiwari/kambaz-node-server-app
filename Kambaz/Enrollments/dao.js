fbb0be48374e89498bab3b566ba54f8c3470e368
Shashwat Tiwari <106163122+shashhwatiwari@users.noreply.github.com>
2025-08-05 23:24:50 +0530

    A5 enrollment server integration fix - attempt 1

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,19 +1,35 @@
+
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
-
 export function enrollUserInCourse(userId, courseId) {
     const { enrollments } = Database;
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-    return 200;
+
+
+    const existingEnrollment = enrollments.find(
+        (enrollment) => enrollment.course === courseId && enrollment.user === userId
+    );
+
+    if (existingEnrollment) {
+        return existingEnrollment; // Return existing enrollment
+    }
+
+
+    const newEnrollment = { _id: uuidv4(), user: userId, course: courseId };
+    enrollments.push(newEnrollment);
+    return newEnrollment; // Return the new enrollment object
 }
 
 export function unenrollUserInCourse(userId, courseId) {
     const { enrollments } = Database;
+    const initialLength = enrollments.length;
+
     Database.enrollments = enrollments.filter(
         (enrollment) => !(enrollment.course === courseId && enrollment.user === userId)
     );
-    return 200;
+
+
+    return Database.enrollments.length < initialLength ? 200 : 404;
 }
 
 export function getEnrollments() {
--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,23 +1,54 @@
-import * as dao from "./dao.js";
-import * as enrollmentsDao from "../Enrollments/dao.js";
+// import * as dao from "./dao.js";
+// import * as enrollmentsDao from "../Enrollments/dao.js";
+//
+// export default function EnrollmentsRoutes(app) {
+//     app.get("/api/enrollments", (req, res) => {
+//         const enrollments = enrollmentsDao.getEnrollments();
+//         res.send(enrollments);
+//     });
+//
+//     app.post("/api/enrollments/:uid/:cid", (req, res) => {
+//         const {uid, cid } = req.params;
+//         const status = dao.enrollUserInCourse(uid, cid);
+//         res.sendStatus(status);
+//     });
+//
+//     app.delete("/api/enrollments/:uid/:cid", (req, res) => {
+//         const {uid, cid } = req.params;
+//         const status = dao.unenrollUserInCourse(uid, cid);
+//         res.sendStatus(status);
+//     });
+// }
+
+import * as enrollmentsDao from "./dao.js";
 
 export default function EnrollmentsRoutes(app) {
     app.get("/api/enrollments", (req, res) => {
         const enrollments = enrollmentsDao.getEnrollments();
-        res.send(enrollments);
+        res.json(enrollments);
     });
 
     app.post("/api/enrollments/:uid/:cid", (req, res) => {
-        const {uid, cid } = req.params;
-        const status = dao.enrollUserInCourse(uid, cid);
-        res.sendStatus(status);
+        const { uid, cid } = req.params;
+        try {
+            const newEnrollment = enrollmentsDao.enrollUserInCourse(uid, cid);
+            res.json(newEnrollment);  // Return the enrollment object
+        } catch (error) {
+            res.status(500).json({ error: "Failed to enroll user" });
+        }
     });
 
     app.delete("/api/enrollments/:uid/:cid", (req, res) => {
-        const {uid, cid } = req.params;
-        const status = dao.unenrollUserInCourse(uid, cid);
-
-
-        res.sendStatus(status);
+        const { uid, cid } = req.params;
+        try {
+            const status = enrollmentsDao.unenrollUserInCourse(uid, cid);
+            if (status === 200) {
+                res.json({ message: "Successfully unenrolled" });
+            } else {
+                res.status(404).json({ error: "Enrollment not found" });
+            }
+        } catch (error) {
+            res.status(500).json({ error: "Failed to unenroll user" });
+        }
     });
 }
